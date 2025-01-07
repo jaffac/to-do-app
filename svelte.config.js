69f1909ed43932b18e1320cf34552a740bbbb285
jaffac <jaffaster@gmail.com>
2025-01-07 19:25:44 +0100

    Install svelte-preprocess and update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,9 +2,7 @@ import adapter from '@sveltejs/adapter-cloudflare';
 import preprocess from 'svelte-preprocess';
 
 export default {
-	preprocess: preprocess({
-		/* options */
-	}), // Pass options if needed
+	preprocess: preprocess(),
 	kit: {
 		adapter: adapter()
 	}
